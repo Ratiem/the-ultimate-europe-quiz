2eb099a483bcb7d8ba7291ed685b5f8a19fe425f
Ratiem <ratidzo.makoni@gmail.com>
2022-10-19 12:50:57 +0000

    Add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,45 @@
+/*Event listeners*/
+
+ startButton.addEventListener("click",function() {
+  runGame();
+  myinterval=setInterval(countdown,1000);
+});
+
+
+rulesButton.addEventListener("click",rules);
+
+
+ nextButton.addEventListener("click",function() {
+    nextQuestion();
+    myinterval=setInterval(countdown,1000);
+  });
+
+
+/*Content displayed when the rules button is clicked*/
+
+function rules() { 
+    introArea.innerHTML =
+    `<h3>Rules</h3>
+    <ol id="rules">
+        <li>There are 12 questions</li>
+        <li>there is one possible answer, click one of the two possible answers</li>
+        <li>You will earn one poiint for each correct answer</li>
+        <li>Click next to proceed after each question</li>
+
+        </ol>`;
+        rulesButoon.classList.add("hide")
+    }
+
+
+
+
+
+
+/**
+ * When the Start Quiz button is pressed, the game area appears.
+ * The first question is called.
+ */
+
 function runGame() {
 
 }
@@ -23,5 +65,5 @@ function incrementScore() {
 }
 
 function incrementWrongAnswer() {
-    
+
 }
